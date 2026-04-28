d6f47701bd46d6b75e6a1ca74fc09e5c422bf8df
nexxia74 <nexxia74@gmail.com>
2026-04-28 22:28:14 +0900

    Update sync.js

--- api/sync.js
+++ api/sync.js
@@ -2,12 +2,12 @@ export default async function handler(req, res) {
   try {
     const { sigungu, bjdong, bun, ji } = req.query;
 
-    const serviceKey = process.env.DATA_GO_KR_KEY;
+    const serviceKey = "52a615f1e89d6eeab474b079ae16c421374bbc9abeff7045b3672124edde4253";
 
     const _bun = (bun || '').padStart(4, '0');
     const _ji = (ji || '').padStart(4, '0');
 
-    const url = `https://apis.data.go.kr/1613000/BldRgstService_v2/getBrTitleInfo?serviceKey=${serviceKey}&sigunguCd=${sigungu}&bjdongCd=${bjdong}&platGbCd=0&bun=${_bun}&ji=${_ji}&numOfRows=1000`;
+    const url = `https://apis.data.go.kr/1613000/BldRgstService_v2/getBrTitleInfo?serviceKey=${serviceKey}&sigunguCd=${sigungu}&bjdongCd=${bjdong}&platGbCd=0&bun=${_bun}&ji=${_ji}&numOfRows=100`;
 
     const response = await fetch(url);
     const text = await response.text();
@@ -26,7 +26,6 @@ export default async function handler(req, res) {
           dongNm: get("dongNm"),
           hoNm: get("hoNm"),
           jmNm: get("jmNm"),
-          area: get("area")
         });
       });
     }
